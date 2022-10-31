543d9640fd1c0edf60541e2fd3323cd55210bc9e
Dinukabk <dinukabk@gmail.com>
2022-10-31 18:22:14 +0530

    update

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ import express from 'express';
 
 import bodyParser from 'body-parser';
 
+import userRoutes from './routes/users.js'
+
 const app = express();
 const PORT = 5000;
 
-app.get('/',(req,res) =>
+app.get('/',(req,res) =>{res.send("hello fron homepage ");})
 
-{
-    console.log('[test]');
-    res.send("hello fron homepage ");
-})
+app.use('/users',userRoutes);
 
 app.use(bodyParser.json());
 app.listen(PORT, ()=> console.log("app is running"));
+
