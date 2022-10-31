3544c477958adc4d3422e54eadf993e19678c836
Dinukabk <dinukabk@gmail.com>
2022-10-31 16:52:37 +0530

    new

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 import express from 'express';
-const express = require('express');
+
+import bodyParser from 'body-parser';
+
+const app = express();
+const PORT = 5000;
+
+app.get('/',(req,res) =>
+
+{
+    console.log('[test]');
+    res.send("hello fron homepage ");
+})
+
+app.use(bodyParser.json());
+app.listen(PORT, ()=> console.log("app is running"));
